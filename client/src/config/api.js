f720f05f085c95fe371a90a5f1fbec83cb70cb9e
e-mustafa <eng.mustafax@gmail.com>
2024-06-22 01:09:16 +0300

    edit domain

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
 const Api = axios.create({
-	baseURL: 'http://localhost:5000/api',
+	baseURL: `${domainBack}/api`,
 	headers: { 'Content-Type': 'application/json' },
 	withCredentials: true, // if there is cookies send it with request
 });
 export default Api;
 
-
-
 // frontend domain
 export const domainFront = 'http://localhost:3000';
-export const domainBack = 'http://localhost:5000';
+export const domainBack = process.env.DOMAIN || 'http://localhost:5000';
