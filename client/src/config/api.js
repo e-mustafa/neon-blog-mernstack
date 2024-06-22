6cb23520400dd57b12aef002176b432fde5d2050
e-mustafa <eng.mustafax@gmail.com>
2024-06-22 04:02:26 +0300

    edit domain

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-	baseURL: `'https://neon-blog-mernstack.onrender.com/api'`,
+	baseURL: `https://neon-blog-mernstack.onrender.com/api`,
 	headers: { 'Content-Type': 'application/json' },
 	withCredentials: true, // if there is cookies send it with request
 });
